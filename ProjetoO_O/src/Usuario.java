65c49de4c6713e4bf32b6b16e530cd7da558919b
Matheus Mösken Diegues <matheus.diegues@fatec.sp.gov.br>
2024-08-20 21:45:47 -0300

    feat: Adicionar atributos e métodos de acesso à classe Usuario
    Adiciona os atributos nome, email, idade e endereço à classe Usuario, juntamente com os métodos de acesso correspondentes. Isso permite que os dados do usuário sejam armazenados e acessados de forma adequada.

--- ProjetoO_O/src/Usuario.java
+++ ProjetoO_O/src/Usuario.java
@@ -9,4 +9,57 @@
  */
 public class Usuario {
     //declarar atributos da classe usuario
+    private String nome;
+    private String email;
+    private int idade;
+    private String endereço;
+
+    //construtor da classe usuario
+    //inicialização dos valores dos atributos
+    public Usuario() {
+        this("","",0,"");
+    }
+
+    public Usuario(String nome, String email, int idade, String endereço) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+        this.endereço = endereço;
+    }
+    // enclapsulamento dos atributos
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+    
+    
+    
 }
